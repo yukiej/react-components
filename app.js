9da203408d22a2b0c20813038b199b08aebbad39
Yuki Jung <yukiej@gmail.com>
2018-10-01 12:09:16 -0700

    Create and render nested React components

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 // TODO
+//Create GroceryList component that contains unordered list of 2 grocery store items
+var GroceryList = (item1, item2) => (
+  <ul>
+    <Apple />
+    <Squash />
+  </ul>
+);
+
+var Squash = () => (
+  <li>I'm a zucchini</li>
+);
+
+var Apple = () => (
+  <li>I'm an apple</li>
+);
+  
+var GroceryListItem = (item) => (
+  <li>{item}}</li>
+)
+  
+
+//Render GroceryList component to the div tag in index.html with an id of app
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
